38a4b8b6e2895f872abf3936f2bb50913dd55df5
John Opaluwa <johnopaluwa@gmail.com>
2022-06-08 01:08:33 +0200

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { sortBy } from "lodash";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  React.useEffect(() => {
+    fetch("https://hedge-crypto-backend.herokuapp.com/crypto")
+      .then((response) => response.json())
+      // 4. Setting *dogImage* to the image url that we received from the response above
+      .then((data) => {
+        const sortedData = data.map((mm) => ({
+          ...mm,
+          value: sortBy(mm.value, [
+            function (o) {
+              return o.url;
+            },
+          ]),
+        }));
+
+        setData(sortedData);
+      });
+  }, []);
+
+  console.warn(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ marginLeft: 20 }}>
+      {data.map((s, _i) => {
+        return (
+          <div>
+            <details>
+              <summary>
+                <h3 style={{ color: "blue" }}> {s.name}</h3>
+              </summary>
+              <div style={{ marginLeft: 50 }}>
+                {s.value.map((m) => {
+                  return (
+                    <p>
+                      <a
+                        target="_blank"
+                        style={m.url ? { color: "green" } : { color: "red" }}
+                        href={m.url}
+                        rel="noreferrer"
+                      >
+                        {m.name}
+                      </a>
+                    </p>
+                  );
+                })}
+              </div>
+            </details>
+          </div>
+        );
+      })}
     </div>
   );
 }
